46437f20aaa5f961b042660066783d44391de277
vivpeng <vivianpeng5211@gmail.com>
2026-08-15 19:14:11 -0700

    remove project title

--- my-site/src/components/Projects.jsx
+++ my-site/src/components/Projects.jsx
@@ -433,7 +433,7 @@ function Projects() {
                                 }
                             `}
                         >
-                            {project.title}
+
                         </span>
                     </button>
                 ))}
